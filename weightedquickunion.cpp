2a3a8c1d660b00078974a30d14fc3f205ce85c2c
Kuat Abylkassymov <kuatabylkasymov@gmail.com>
2019-12-12 18:50:07 +0600

    change graph

--- weightedquickunion.cpp
+++ weightedquickunion.cpp
@@ -82,6 +82,7 @@ bool WeightedQuickUnion::isCycle(){
     int x  = find(graph->edge[i].src);
     int y  = find(graph->edge[i].dest);
     if (x == y) {
+      std::cout << graph->edge[i].src << graph->edge[i].dest << std::endl;
       return 1;
     }
     Union(x, y);
@@ -104,13 +105,25 @@ WeightedQuickUnion* createWeightedQuickUnion(Graph* graph){
 int main()
 {
 
-  Graph* graph = createGraph(3,3);
+  Graph* graph = createGraph(9,9);
   graph->edge[0].src = 0;
   graph->edge[0].dest = 1;
-  graph->edge[1].src = 1;
+  graph->edge[1].src = 0;
   graph->edge[1].dest = 2;
-  graph->edge[2].src = 0;
-  graph->edge[2].dest = 2;
+  graph->edge[2].src = 1;
+  graph->edge[2].dest = 3;
+  graph->edge[3].src = 3;
+  graph->edge[3].dest = 4;
+  graph->edge[4].src = 2;
+  graph->edge[4].dest = 4;
+  graph->edge[5].src = 4;
+  graph->edge[5].dest = 5;
+  graph->edge[6].src = 5;
+  graph->edge[6].dest = 6;
+  graph->edge[7].src = 6;
+  graph->edge[7].dest = 7;
+  graph->edge[8].src = 7;
+  graph->edge[8].dest = 8;
 
   WeightedQuickUnion* uf = createWeightedQuickUnion(graph);
 
