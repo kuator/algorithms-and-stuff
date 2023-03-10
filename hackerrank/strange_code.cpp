e4dcc0fd9c55e2bcfac2930eedc7bd901c5d2326
evakuator <kuatabylkasymov@gmail.com>
2023-03-10 11:48:31 +0600

    strange_code

--- hackerrank/strange_code.cpp
+++ hackerrank/strange_code.cpp
@@ -1,24 +1,20 @@
 #include <bits/stdc++.h>
+#include <cmath>
 using namespace std;
 
 long strangeCounter(long t) {
-  int value = 3;
-  int time = 1;
-  int value_cnt = value;
-  for (int i = 1; i<=t; i++) {
-    if (i==value+time) {
-      value = value * 2;
-      time = i;
-      value_cnt = value;
-      /* cout << i << " " << value_cnt << endl; */
+  long n = 1L;
+  while (true) {
+    long cur_sum = 3*(pow(2,n)-1);
+    long prev_sum = 3*(pow(2,n-1)-1);
+    long time_at_chunk = 3 * pow(2, n-1);
+    cout << n << " " << cur_sum << " " << prev_sum << " " << time_at_chunk << endl;
+    if (t <= cur_sum) {
+      long result = 1 + time_at_chunk - (t - prev_sum);
+      return result;
     }
-    /* cout << i << " " << value_cnt << endl; */
-    if (i == t) {
-      return value_cnt;
-    }
-    value_cnt--;
+    n++;
   }
-  return value_cnt;
 }
 
 int main (int argc, char *argv[])
